5207a553afc511b59e6e3c732aca6ee327a46b6c
Oliver Clasen <oliver.clasen@alice-dsl.net>
2019-12-10 15:49:36 +0100

    progressbar

--- tweets/translate.py
+++ tweets/translate.py
@@ -5,24 +5,23 @@ import sys
 import time
 import json
 import goslate
+from tqdm import tqdm
 
 
 
 r = csv.reader(open(sys.argv[1]))
 tweets = list(r)
-for tweet in tweets:
+for tweet in tqdm(tweets):
     try:
         translator = Translator()
         translation = translator.translate(tweet[10])
         tweet[10] = translation.text
-        print(tweet[10])
-        time.sleep(3)
+        time.sleep(1)
     except json.decoder.JSONDecodeError:
         print("googleTrans blocked")
         translator = goslate.Goslate()
         translation = translator.translate(tweet[10], 'en')
         tweet[10] = translation
-        print(tweet[10])
-        time.sleep(3)
+        time.sleep(1)
 writer = csv.writer(open("translations/" + sys.argv[1], 'w'))
 writer.writerows(tweets)
