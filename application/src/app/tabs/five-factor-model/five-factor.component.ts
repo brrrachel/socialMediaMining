4287a44538360a32fba575f148fd2f19175fb81a
Oliver Clasen <oliver.clasen@alice-dsl.net>
2020-02-19 15:08:15 +0100

    five factor model is now working/updating and cdu & csu are combined if they appear together

--- application/src/app/tabs/five-factor-model/five-factor.component.ts
+++ application/src/app/tabs/five-factor-model/five-factor.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input, OnChanges, OnInit} from '@angular/core';
-import {AccessService} from "../../services/access.service";
-import {Parties} from "../../models/party.model";
+import {AccessService, fiveFactorModel} from "../../services/access.service";
+import {getColorForParty, Parties} from "../../models/party.model";
 import * as Chart from 'chart.js'
 import {Timespan} from "../../models/time-span.model";
 
@@ -25,6 +25,12 @@ export class FiveFactorComponent implements OnInit, OnChanges {
   chartOptions: Chart.ChartOptions = {
     title: {
       display: true
+    },
+    scale:{
+      ticks: {
+        suggestedMin: 0,
+        suggestedMax: 5
+      }
     }
   };
 
@@ -58,9 +64,58 @@ export class FiveFactorComponent implements OnInit, OnChanges {
 
   ngOnChanges() {
     if (this.chart) {
-      this.chart.data.datasets = this.datasets;
-      this.chart.update();
+      this.getData();
     }
+  }
+
+  hexToRgbA(hex, alpha){
+    let c;
+    if(/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)){
+      c= hex.substring(1).split('');
+      if(c.length== 3){
+        c= [c[0], c[0], c[1], c[1], c[2], c[2]];
+      }
+      c= '0x'+c.join('');
+      return 'rgba('+[(c>>16)&255, (c>>8)&255, c&255].join(',')+',' + alpha + ')';
+    }
+    throw new Error('Bad Hex');
+  }
+
+  getData(){
+    this.accessService.getFiveFactors(this.selectedParties, this.selectedYears)
+      .then(data => this.setChartData(data));
+  }
+
+  setChartData(data: { party: string, factors: fiveFactorModel}[]){
+    let dataSets: RadarChartData[] = [];
+    if(data.length >= 0){
+      for (let partyData of data){
+        console.log(partyData);
+        let partyName = partyData.party;
+        let partyEnum = "";
+        if (partyName === "CDU/CSU"){
+          partyEnum = "CDU"
+        } else {
+          partyEnum = partyName
+        }
+        let fiveFactors = partyData.factors[0];
+        let partyColor = this.hexToRgbA(getColorForParty(partyEnum),"1");
+        let partyColorOpac = this.hexToRgbA(getColorForParty(partyEnum),"0.2");
+        dataSets.push({
+          label: partyName,
+          fill: true,
+          backgroundColor: partyColorOpac,
+          borderColor: partyColor,
+          pointBorderColor: partyColor,
+          pointBackgroundColor: partyColor,
+          data: [fiveFactors.openness, fiveFactors.conscientiousness, fiveFactors.extraversion, fiveFactors.agreeableness, fiveFactors.neuroticism]
+        })
+      }
+    }
+    console.log(this.selectedYears);
+    console.log(dataSets);
+    this.chart.data.datasets = dataSets;
+    this.chart.update();
   }
 
   initChart() {
@@ -74,7 +129,7 @@ export class FiveFactorComponent implements OnInit, OnChanges {
       },
       options: this.chartOptions
     });
-    this.chart.update();
+    this.getData();
   }
 }
 
