2c14b8e551903e61d6dd0a56f39991fe7dfb2803
Rachel <rachel.brabender@student.hpi.de>
2020-03-03 13:36:05 +0100

    removed use of nltk wordnets and dictonaries repreprocessed tweet tokens creation

--- data/preprocessing/preprocessTweets.py
+++ data/preprocessing/preprocessTweets.py
@@ -10,35 +10,16 @@ from nltk.tokenize import TweetTokenizer
 tweet_tokenizer = TweetTokenizer()
 nltk.download('stopwords')
 from nltk.corpus import stopwords
-nltk.download('words')
-from nltk.corpus import words
-nltk.download('wordnet')
-from nltk.corpus import wordnet
 
-stop_words = set(stopwords.words('english'))
-wordlist_lowercased = set([i.lower() for i in words.words()] + [i.lower() for i in wordnet.words()])
+stop_words = set([i.lower() for i in stopwords.words('english')])
 
 file_name = 'preprocessed_tweets.csv'
-
-
-def check_for_unusual_word(word):
-    if len(word) > 1:
-        if (word.startswith('@') or word.startswith('#')) and ((word.count('@') == 1) or (word.count('#') == 1)):
-            return True
-        elif word.count('?') > 0 or word.count('?') > 0 or word.count('?') > 0:
-            return True
-        else:
-            return False
-    else:
-        return False
-
 
 if __name__ == '__main__':
 
     os.chdir('../../setup/querys/')
     if os.path.exists(file_name):
         os.remove(file_name)
-
 
     years = range(2009, 2020)
     months = range(1, 13)
@@ -80,23 +61,20 @@ if __name__ == '__main__':
                     result = re.sub(r'/\xc3/\xa4', '?', result)
                     result = re.sub(r'/\xc3/\xb6', '?', result)
 
-                    # extract hashtags, references and words with umlauts
+                    # create tokens
                     word_tokens = tweet_tokenizer.tokenize(result)
-                    unusual_words = []
-                    # unusual_words = [w for w in word_tokens if check_for_unusual_word(w)]
 
                     # remove stopwords
                     filtered_sentence = [w for w in word_tokens if not w in stop_words]
 
-                    # remain meaningful words
+                    # filter remain words
                     filtered = list(filter(None, filtered_sentence))
                     meaningful = [w for w in filtered if (len(w) > 1) and (w.isdigit() != True) and not w.startswith('www') and not w.endswith('html')]
-                    # (w in wordlist_lowercased) and
 
                     if meaningful:
                         with open(file_name, 'a') as fp:
                             wr = csv.writer(fp, dialect='excel')
-                            result = [index, party, year, month, ' '.join(meaningful + unusual_words).encode('utf-8')]
+                            result = [index, party, year, month, ' '.join(meaningful).encode('utf-8')]
                             wr.writerow(result)
                         index += 1
                 else:
